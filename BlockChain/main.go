edba19cfb8949f7b42a31760b8913d4d88382543
osho-20 <yashjainiiita@gmail.com>
2023-07-10 18:25:52 +0530

    new

--- BlockChain/main.go
+++ BlockChain/main.go
@@ -90,31 +90,11 @@ func main() {
 		app.Run()
 	}()
 
-	// Flag handling for BlockChain Server
-	port1 := flag.Uint("port", 5000, "TCP Port Number for BlockChain Server")
-	bind = flag.String("bind", "", "Bind address for the server")
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-		flag.PrintDefaults()
-	}
-	flag.Parse()
-
-	if flag.NArg() > 0 {
-		fmt.Fprintf(os.Stderr, "Error: Unexpected positional arguments\n")
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	if *bind != "" {
-		// Handle the bind address logic here
-		fmt.Printf("Using bind address for BlockChain Server: %s\n", *bind)
-	}
-
 	// Start BlockChain Server in a goroutine
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		app := server.BCServer(uint16(*port1))
+		app := server.BCServer(uint16(*port))
 		fmt.Println(app)
 		app.Run()
 	}()
